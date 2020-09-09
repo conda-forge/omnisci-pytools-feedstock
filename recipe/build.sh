4b2d1089e9732e81bf7aaf5e0a36de41107c2243
Anthony Scopatz <scopatz@gmail.com>
2020-09-09 10:49:23 -0700

    npm package

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
+# names and versions don't match
+export pkg_name="jupyterlab-omnisci"
+export pkg_version="2.0.0"
+
 $PYTHON -m pip install . -vv
-npm pack ${PKG_NAME}@${PKG_VERSION}
+npm pack ${pkg_name}@${pkg_version}
 mkdir -p ${PREFIX}/share/jupyter/lab/extensions/js
-cp ${PKG_NAME}-${PKG_VERSION}.tgz ${PREFIX}/share/jupyter/lab/extensions/js
+cp ${pkg_name}-${pkg_version}.tgz ${PREFIX}/share/jupyter/lab/extensions/js
